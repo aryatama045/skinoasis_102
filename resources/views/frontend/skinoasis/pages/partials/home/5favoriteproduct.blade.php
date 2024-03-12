da57f952b05bdaf67c789e6983ea77f62f0559fd
Aryatama <18211338+aryatama045@users.noreply.github.com>
2024-03-12 14:29:56 +0700

    Update 5favoriteproduct.blade.php

--- resources/views/frontend/skinoasis/pages/partials/home/5favoriteproduct.blade.php
+++ resources/views/frontend/skinoasis/pages/partials/home/5favoriteproduct.blade.php
@@ -59,7 +59,7 @@
                     }'>
                     @php
                         $trending_products = getSetting('top_trending_products') != null ? json_decode(getSetting('top_trending_products')) : [];
-                        $products = \App\Models\Product::whereIn('id', $trending_products)->get();
+                        $products = \App\Models\Product::whereIn('products.id', $trending_products)->get();
                     @endphp
 
                     @foreach ($products as $product)
